a98b60595383aa98e9a5320b0a162f3e7a574d77
david_tupang <davidprdmnsimatupang@gmail.com>
2024-11-16 11:46:24 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import IncidentsList from './components/IncidentsList';
 import CreateIncident from './components/CreateIncident';
 import WorkOrdersList from './components/WorkOrdersList';
 import CreateWorkOrder from './components/CreateWorkOrder';
-import Login from './components/login';
+import Login from './components/Login';
 import './App.css';
 
 axios.defaults.baseURL = 'http://localhost:3000/api';
